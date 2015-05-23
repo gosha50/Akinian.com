6ee4f17a8b9209ce68855aff9b5236e4550ba94f
George Akinian <akinian@me.com>
2015-05-22 23:36:35 -0700

    col

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
     <header id="top" class="header">
         <div class= "text-vertical-center">
             <h1>george akinian</h1>
-            <h3>Web Solutions &amp; Software Development</h3>
+          <!--   <h3>Web Solutions &amp; Software Development</h3> -->
             <br>
             <a href="#about" class="btn btn-dark btn-lg">Find Out More</a>
         </div>
