68d2d3af7160011a6913f0f7c677fc645f2de2f1
George Akinian <akinian@me.com>
2015-08-27 11:22:39 -0700

    took out fa fa bars in the open menu.

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -2,7 +2,7 @@
     <!-- Navigation -->
     <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle"><i class="fa fa-bars"></i></a>
     <nav id="sidebar-wrapper">
-        <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-bars"></i></a>
+       <!--  <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle"><i class="fa fa-bars"></i></a> -->
          <ul class="sidebar-nav">
 
             <?php foreach ($page->menu_items()->toStructure() as $item): ?>
