81f1c6266ea6697208cbbb18d16eac19c3f43813
George Akinian <akinian@me.com>
2016-02-25 11:44:25 -0800

    ya.

--- site/templates/home-one.php
+++ site/templates/home-one.php
@@ -1,1 +1,39 @@
-hello World
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo $page->title()->html() ?></title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/home-one.css">
+</head>
+
+
+
+<body>	
+
+	<div id="main" class="contact" style="background-image: url(<?php echo $page->file($page->hero_image())->url() ?>)">
+		
+		<div class="intro">
+			<div>
+				<h1 id="name">
+				    HI, <br>
+				    I'M GEORGE AKINIAN.
+				</h1>
+			</div>
+	
+
+			<p>Web-Developer with the toolkit. If you need a website. I will do everything in my power to deliver to you the artwork that will expand your buisness into cyberspace for the purpose to increase conversion rates. Drop me a line or two on my email <a href="mailto:name@akinian.com">hello@akinian.com</a>, or give me a ring at <a href="tel:1-360-609-9987">1-360-609-9987.</a>  </p>
+		</div>
+
+	</div>
+
+</body>
+
+</html>
